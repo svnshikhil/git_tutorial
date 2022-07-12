eb2417a777a77869c02e829239486100089cbfa2
Shikhil S <shikhil.s@pickles.com.au>
2022-07-12 16:04:21 +0530

    New routes

--- node.js
+++ node.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/2222222', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/11111', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/3333333', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
